591fa34b4f8698c7901c24e41db5f0b0a57fc0b5
Vanya A. Sergeev <v@sergeev.io>
2016-09-25 03:44:55 -0700

    fix email address format in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version='2.1',
     description='A portable, lightweight MessagePack serializer and deserializer written in pure Python.',
     author='vsergeev',
-    author_email='v at sergeev.io',
+    author_email='v@sergeev.io',
     url='https://github.com/vsergeev/u-msgpack-python',
     py_modules=['umsgpack'],
     long_description="""u-msgpack-python is a lightweight `MessagePack <http://msgpack.org/>`_ serializer and deserializer module written in pure Python, compatible with both Python 2 and Python 3, as well as CPython and PyPy implementations of Python. u-msgpack-python is fully compliant with the latest `MessagePack specification <https://github.com/msgpack/msgpack/blob/master/spec.md>`_. In particular, it supports the new binary, UTF-8 string, and application-defined ext types. See https://github.com/vsergeev/u-msgpack-python for more information.""",
