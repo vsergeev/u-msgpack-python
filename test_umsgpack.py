23bb18b024a9cefa876369f4e939ac24be2d10a0
Gabe Appleton <gabe@gabeappleton.me>
2019-04-18 16:05:16 -0700

    Attempt correction of export check

--- test_umsgpack.py
+++ test_umsgpack.py
@@ -608,8 +608,8 @@ class TestUmsgpack(unittest.TestCase):
         exported_vars = list(filter(lambda x: not x.startswith("_"),
                                     dir(umsgpack)))
         # Ignore imports
-        exported_vars = list(filter(lambda x: x != "struct" and x != "collections" and x != "datetime" and x !=
-                                    "sys" and x != "io" and x != "xrange", exported_vars))
+        exported_vars = list(filter(lambda x: x not in ("struct", "Hashable", "datetime", "version_info", "float_info",
+                                                        "Bytes", "xrange", "OrderedDict"), exported_vars))
 
         self.assertTrue(len(exported_vars) == len(exported_vars_test_vector))
         for var in exported_vars_test_vector:
